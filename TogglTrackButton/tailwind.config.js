2a7ec44abd79a0f36fbdbdfccc84833f2b31c0b4
紅坂柚月 <akasaka@ydk.vc>
2024-10-28 18:20:39 +0900

    [add] Flowbite React

--- TogglTrackButton/tailwind.config.js
+++ TogglTrackButton/tailwind.config.js
@@ -1,12 +1,17 @@
+const flowbite = require('flowbite-react/tailwind')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     './src/renderer/src/**/*.{vue,js,ts,jsx,tsx}',
     './src/renderer/index.html',
+    flowbite.content(),
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    flowbite.plugin(),
+  ],
 }
 
