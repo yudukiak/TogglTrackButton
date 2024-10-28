c76fbf7e38a39dc11a05690c53815f6b7a60df8a
紅坂柚月 <akasaka@ydk.vc>
2024-10-28 18:19:17 +0900

    [add] Tailwind CSS

---
+++ TogglTrackButton/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/renderer/src/**/*.{vue,js,ts,jsx,tsx}',
+    './src/renderer/index.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
