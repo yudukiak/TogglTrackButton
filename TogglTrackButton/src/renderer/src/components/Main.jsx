0f4f12d663032e574e72c6bfa4fecc28b860ab86
紅坂柚月 <akasaka@ydk.vc>
2024-10-30 16:32:42 +0900

    [update] TimerCardからMainへデータを送れるようにする

--- TogglTrackButton/src/renderer/src/components/Main.jsx
+++ TogglTrackButton/src/renderer/src/components/Main.jsx
@@ -57,6 +57,7 @@ export default function Main() {
       <TimerCard
         projects={projects}
         currentProject={currentProject}
+        onCurrentProject={handleCurrentProject}
       />
     </main>
   )
